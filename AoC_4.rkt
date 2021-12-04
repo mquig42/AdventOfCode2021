183c9a98fa0e1105e12e7f2074a9b56f3a0ddb52
mquig42 <mquig42@users.noreply.github.com>
2021-12-04 14:44:01 -0500

    Solved day 4

---
+++ AoC_4.rkt
@@ -0,0 +1,175 @@
+;;;AoC_4.txt
+;;;2021-12-04
+;;;Mike Quigley
+
+;;;Today we're playing bingo with a squid. Might be at a disadvantage here,
+;;;he can hold like ten markers at once.
+;;;Input is a sequence of numbers, followed by a sequence of bingo cards
+;;;For part 1, find the winning card and its score
+;;;For part 2, find the losing card and its score. 
+
+;;;For the first time, I'll need to read the input in a more complicated way
+;;;than just putting everything into one big list.
+;;;There are three challenges here
+;;;1. Read the draw sequence and all cards, and represent them in memory
+;;;2. Figure out when in the draw sequence each card wins
+;;;3. Calculate the final score of any given card
+
+;First, use full-on Racket instead of SICP. Need some extra io functions.
+#lang racket
+
+;To read one line of a Bingo card, simply read 5 items into a list
+(define (read-card-line file)
+  (list (read file)
+        (read file)
+        (read file)
+        (read file)
+        (read file)))
+
+;To read a whole card, call read-card-line five times
+(define (read-card file)
+  (list (read-card-line file)
+        (read-card-line file)
+        (read-card-line file)
+        (read-card-line file)
+        (read-card-line file)))
+
+;Reads a fixed number of cards. That way we don't need to check for EOF
+(define (read-n-cards n file)
+  (if (= 0 n) null
+      (cons (read-card file) (read-n-cards (- n 1) file))))
+
+;Generate a sequence of integers from start to end
+(define (make-seq start end)
+  (if (> start end) null
+      (cons start (make-seq (+ start 1) end))))
+
+;Simple linear search. Returns position of n
+(define (position-of n lis)
+  (define (iter acc n lis)
+    (if (= n (car lis)) acc
+        (iter (+ acc 1) n (cdr lis))))
+  (iter 0 n lis))
+
+;Returns nth item of a list
+(define (item-at n lis)
+  (if (= n 0) (car lis)
+      (item-at (- n 1) (cdr lis))))
+
+;Might change draw-sequence from a list to a dictionary later.
+;Making wrappers now to make that easier
+(define (draw-pos n)
+  (position-of n draw-sequence))
+
+(define (nth-draw n)
+  (item-at n draw-sequence))
+
+;Flexible function that can find the maximum or minimum value of a list
+(define (list-val-helper initial comparison lis)
+  (cond ((null? lis) initial)
+        ((comparison (car lis) initial)
+         (list-val-helper (car lis) comparison (cdr lis)))
+        (else (list-val-helper initial comparison (cdr lis)))))
+
+;Returns maximum value of a list
+(define (max-val lis)
+  (list-val-helper 0 > lis))
+
+;Returns minimum value of a list
+(define (min-val lis)
+  (list-val-helper 99999 < lis))
+
+;Returns the position of the lowest item in a list
+(define (pos-of-lowest lis)
+  (define (iter pos pos-lowest lowest lis)
+    (cond ((null? lis) pos-lowest)
+          ((< (car lis) lowest)
+           (iter (+ pos 1) pos (car lis) (cdr lis)))
+          (else (iter (+ pos 1) pos-lowest lowest (cdr lis)))))
+  (iter 0 0 99999 lis))
+
+;Returns the position of the highest item in a list
+;This is a near-exact diplicate of pos-of-lowest, and should be combined
+(define (pos-of-highest lis)
+  (define (iter pos pos-highest highest lis)
+    (cond ((null? lis) pos-highest)
+          ((> (car lis) highest)
+           (iter (+ pos 1) pos (car lis) (cdr lis)))
+          (else (iter (+ pos 1) pos-highest highest (cdr lis)))))
+  (iter 0 0 0 lis))
+
+;Returns nth row of card
+(define (row n card)
+  (item-at n card))
+
+;Returns nth column of card
+(define (col n card)
+  (map (λ (x) (item-at n (row x card))) list5))
+
+;Returns a list of all rows and columns of a card
+(define (enumerate card)
+  (append card (map (λ (x) (col x card)) list5)))
+
+;Returns the draw-position of the last number to be drawn in this list
+(define (time-to-win lis)
+  (max-val (map draw-pos lis)))
+
+;Returns the draw-position of the last number to be drawn in this card
+(define (time-to-win-card card)
+  (min-val (map time-to-win (enumerate card))))
+
+;Returns the index of the winning card
+(define (pos-winning-card)
+  (pos-of-lowest (map time-to-win-card cards)))
+
+;Returns the index of the losing card (last card to win)
+(define (pos-losing-card)
+  (pos-of-highest (map time-to-win-card cards)))
+
+;Finds the final score of a card at the time that it wins,
+;assuming no other cards win first
+(define (score card)
+  (define (sum-unmarked acc num-draws card)
+    (cond ((null? card) acc)
+          ((< num-draws (draw-pos (car card)))
+           (sum-unmarked (+ acc (car card)) num-draws (cdr card)))
+          (else (sum-unmarked acc num-draws (cdr card)))))
+  (let ((num-draws (time-to-win-card card)))
+    (* (sum-unmarked 0 num-draws (flatten card)) (nth-draw num-draws))))
+
+;;Entry Point ==================================================================
+
+(define filename "Input4.txt")
+(define number-of-cards 100)
+(define list5 '(0 1 2 3 4))
+(define input-file (open-input-file filename))
+
+;First line is the draw sequence. Read it into a list.
+(define draw-sequence
+  (map (λ (x) (string->number x))
+       (string-split (string-trim (read-line input-file)) ",")))
+
+;Now read all cards
+(define cards (read-n-cards number-of-cards input-file))
+
+(close-input-port input-file)
+
+;Find and display output
+(display "Part 1:")
+(newline)
+(display "Winning Card: ")
+(pos-winning-card)
+(display "       Draws: ")
+(time-to-win-card (item-at (pos-winning-card) cards))
+(display "       Score: ")
+(score (item-at (pos-winning-card) cards))
+
+(newline)
+(display "Part 2:")
+(newline)
+(display " Losing Card: ")
+(pos-losing-card)
+(display "       Draws: ")
+(time-to-win-card (item-at (pos-losing-card) cards))
+(display "       Score: ")
+(score (item-at (pos-losing-card) cards))

1e63fdf9c0caf5a688e660f4e14bcd21aaf46c0b
mquig42 <mquig42@users.noreply.github.com>
2021-12-07 12:09:25 -0500

    Improved day 7
    Used library functions to significantly shorten program. Changed movement-cost-2 from brute force to math

--- AoC_07.rkt
+++ AoC_07.rkt
@@ -10,49 +10,28 @@
 ;;;movement-cost functions, it was easy to refactor.
 ;;;I'm starting to like functional programming.
 
-;;;Right now my part 2 solution takes over 30 seconds to run. This could be
-;;;improved by precomputing all the movement costs and making a lookup table.
-;;;Also, didn't Euler come up with an algebraic solution? Or was it Gauss?
-;;;That would be even better.
+;;;Part 2 is now optimized. It used to do the calculation iteratively,
+;;;which took over 30 seconds to run. Thanks to Gauss, that's down to 175ms.
 
 #lang racket
-;Flexible function that can find the maximum or minimum value of a list
-(define (list-val-helper initial comparison lst)
-  (cond ((null? lst) initial)
-        ((comparison (car lst) initial)
-         (list-val-helper (car lst) comparison (cdr lst)))
-        (else (list-val-helper initial comparison (cdr lst)))))
-
-;Returns maximum value of a list
-(define (max-val lst)
-  (list-val-helper (car lst) > lst))
-
-;Returns minimum value of a list
-(define (min-val lst)
-  (list-val-helper (car lst) < lst))
-
 ;Calculates the fuel required to move all the submarines in lst to position dest
 (define (total-movement-cost lst dest cost)
-  (define (iter acc lst)
-    (if (null? lst) acc
-        (iter (+ acc (cost (car lst) dest)) (cdr lst))))
-  (iter 0 lst))
+  (foldl (λ (pos acc) (+ acc (cost pos dest))) 0 lst))
 
 ;Calculates total movement cost for every possible destination, returns minimum
 (define (solve lst cost)
-  (min-val (map (λ (x) (total-movement-cost lst x cost))
-                (range (min-val lst) (+ 1 (max-val lst))))))
+  (argmin identity (map (λ (x) (total-movement-cost lst x cost))
+                        (range (argmin identity lst)
+                               (+ 1 (argmax identity lst))))))
 
 ;Movement cost calculation for part 1
 (define (movement-cost-1 from to)
   (abs (- from to)))
 
-;Movement cost calculation for part 2
+;Movement cost for part 2
 (define (movement-cost-2 from to)
-  (define (iter acc distance)
-    (if (= 0 distance) acc
-        (iter (+ acc distance) (- distance 1))))
-  (iter 0 (abs (- from to))))
+  (let ((dist (+ 1 (abs (- from to)))))
+        (/ (* dist (- dist 1)) 2)))
 
 ;;Entry Point ==================================================================
 (define input-file (open-input-file "Input07.txt"))

1af687a6fd1a40e7433087337f3d342fe9f226b6
mquig42 <mquig42@users.noreply.github.com>
2021-12-15 20:28:01 -0500

    Update AoC_15.rkt
    Refactor to make part 2 easier

--- AoC_15.rkt
+++ AoC_15.rkt
@@ -19,75 +19,82 @@
 ;;;to visit.
 
 #lang racket
-;Read input file into a list
+(require data/heap)
+
+;;Generate starting grid by reading file
 (define (read-lines file)
+  (define (string->digits str)
+    (map (λ (x) (- (char->integer x) (char->integer #\0)))
+         (string->list str)))
   (let ((line (read-line file)))
     (if (eof-object? line) null
         (cons (string->digits (string-trim line))
               (read-lines file)))))
 
-;Convert a digit string to a list of numbers
-(define (string->digits str)
-  (map (λ (x) (- (char->integer x) (char->integer #\0)))
-       (string->list str)))
+;;Finds the total risk of the best path from top left to bottom right of grid
+;;Includes Dijkstra's algorithm and some helper functions for getting data
+(define (solve input)
+  (let ((max-row (length input))
+        (max-col (length (car input)))
+        (max-n (* (length input) (length (car input))))
+        (distances (make-vector
+                    (* (length input) (length (car input))) +inf.0)))
+    
+    ;;Getters
+    ;Returns the value of the given point in grid. false for invalid coords
+    (define (get-value row column)
+      (cond ((and (>= row 0)
+                  (>= column 0)
+                  (< row max-row)
+                  (< column max-col))
+             (list-ref (list-ref input row) column))
+            (else false)))
 
-;;Getters
-;Returns the value of the given point in grid. false for invalid coords
-(define (get-value row column)
-  (cond ((and (>= row 0)
-              (>= column 0)
-              (< row max-row)
-              (< column max-col))
-         (list-ref (list-ref input row) column))
-        (else false)))
+    ;It may be helpful to have a unique ID for each point in the grid
+    ;as a single value, not a coordinate pair. Use an integer, like this
+    ;'(0 1 2)
+    ;'(3 4 5)
+    ;'(6 7 8)
+    (define (get-value-n n)
+      (get-value (floor (/ n max-col)) (modulo n max-col)))
 
-;It may be helpful to have a unique ID for each point in the grid
-;as a single value, not a coordinate pair. Use an integer, like this
-;'(0 1 2)
-;'(3 4 5)
-;'(6 7 8)
-(define (get-value-n n)
-  (get-value (floor (/ n max-col)) (modulo n max-col)))
+    ;Calculate ID number for row and column
+    (define (get-n row column)
+      (+ (* row max-col) column))
 
-;Calculate ID number for row and column
-(define (get-n row column)
-  (+ (* row max-col) column))
+    ;Get a list of neighbouring id numbers
+    (define (get-neighbours n)
+      (let ((row (floor (/ n max-col)))
+            (col (modulo n max-col)))
+        (map (λ (x) (get-n (car x) (cdr x)))
+             (filter (λ (y) (get-value (car y) (cdr y)))
+                     (list (cons (- row 1) col)
+                           (cons (+ row 1) col)
+                           (cons row (- col 1))
+                           (cons row (+ col 1)))))))
 
-;Get a list of neighbouring id numbers
-(define (get-neighbours n)
-  (let ((row (floor (/ n max-col)))
-        (col (modulo n max-col)))
-    (map (λ (x) (get-n (car x) (cdr x)))
-         (filter (λ (y) (get-value (car y) (cdr y)))
-                 (list (cons (- row 1) col)
-                       (cons (+ row 1) col)
-                       (cons row (- col 1))
-                       (cons row (+ col 1)))))))
+    ;Dijkstra's Algorithm for shortest path
+    (define (dijkstra current dest unvisited)
+      (for-each (λ (x) (vector-set! distances x
+                                    (min (vector-ref distances x)
+                                         (+ (vector-ref distances current)
+                                            (get-value-n x)))))
+                (get-neighbours current))
+      (if (= current dest) (vector-ref distances current)
+          (let ((unvisited-c (set-remove unvisited current)))
+            (dijkstra (argmin (λ (x) (vector-ref distances x))
+                              (set->list unvisited-c))
+                      dest
+                      unvisited-c))))
 
-;Dijkstra's Algorithm for shortest path
-(define (dijkstra current dest unvisited)
-  (for-each (λ (x) (vector-set! distances x
-                                (min (vector-ref distances x)
-                                     (+ (vector-ref distances current)
-                                        (get-value-n x)))))
-            (get-neighbours current))
-  (if (= current dest) (vector-ref distances current)
-      (let ((unvisited-c (set-remove unvisited current)))
-      (dijkstra (argmin (λ (x) (vector-ref distances x))
-                        (set->list unvisited-c))
-                dest
-                unvisited-c))))
-       
+    (vector-set! distances 0 0)
+    (dijkstra 0 (- max-n 1) (list->set (range max-n)))))
 
 ;;Read input
 (define input-file (open-input-file "Input15.txt"))
 (define input (read-lines input-file))
 (close-input-port input-file)
-(define max-row (length input))
-(define max-col (length (car input)))
-(define max-n (* max-row max-col))
 
-(define distances (make-vector max-n +inf.0))
-(vector-set! distances 0 0)
-
-(time (dijkstra 0 (- max-n 1) (list->set (range max-n))))
+;;Display output
+(display "Part 1: ")
+(solve input)
